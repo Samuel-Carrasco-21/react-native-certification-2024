25c5894958f4d9f50569fafb66d7f3156f898f58
Paul Landaeta <paulwilkerlf@gmail.com>
2024-07-09 11:17:53 -0400

    posts

---
+++ projects/c_instagram/src/data/posts.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        id: 1,
+        user: {
+            id: 1,
+            username: 'marian',
+            imageUrl: 'https://randomuser.me/api/portraits/med/women/1.jpg'
+        },
+        likes: 10,
+        text: 'Soooooo cute',
+        comments: 5,
+        imageUrl: 'https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg',
+        commentsData: [
+            {
+                id: 1,
+                user: {
+                    id: 2,
+                    username: 'Brenda',
+                    imageUrl: 'https://randomuser.me/api/portraits/med/women/6.jpg',
+                },
+                text: 'So cute!'
+            },
+            {
+                id: 2,
+                user: {
+                    id: 2,
+                    username: 'Alexia',
+                    imageUrl: 'https://randomuser.me/api/portraits/med/women/10.jpg',
+                },
+                text: 'ahhhh!!!'
+            },
+            {
+                id: 3,
+                user: {
+                    id: 2,
+                    username: 'Mishel',
+                    imageUrl: 'https://randomuser.me/api/portraits/med/women/4.jpg',
+                },
+                text: 'I want one!'
+            }
+        ]
+
+    }
+]
