0533a3cf9430c80e15a5a941374f2cb37caa2f1d
Paul Landaeta <paulwilkerlf@gmail.com>
2024-07-17 21:35:20 -0400

    firebase login

--- projects/c_instagram/src/config/firebase.js
+++ projects/c_instagram/src/config/firebase.js
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyACnUYSt1H6BXQ9mDk6qq019M3cImDEea0",
+  authDomain: "c-instagram-c072e.firebaseapp.com",
+  projectId: "c-instagram-c072e",
+  storageBucket: "c-instagram-c072e.appspot.com",
+  messagingSenderId: "157624323621",
+  appId: "1:157624323621:web:b5d2363997f36233bdb9cc",
+  measurementId: "G-LXX5B4SF0Z",
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
